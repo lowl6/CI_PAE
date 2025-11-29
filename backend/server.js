f9b767702d6971a1f52f8c4a08c8e623b8c1449f
luchen022 <3108169667@qq.com>
2025-11-29 21:15:50 +0800

    显示正确的IP

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 const app = require('./app')
 const config = require('../config')
+const os = require('os')
 
 const port = config.port || 3001
 const host = '0.0.0.0' // 允许局域网访问
 
+// 获取本机IP地址
+function getServerIP() {
+  const interfaces = os.networkInterfaces()
+  for (const name of Object.keys(interfaces)) {
+    for (const interface of interfaces[name]) {
+      // 跳过内部地址和非IPv4地址
+      if (interface.family === 'IPv4' && !interface.internal) {
+        return interface.address
+      }
+    }
+  }
+  return 'localhost'
+}
+
+const serverIP = getServerIP()
+
 app.listen(port, host, () => {
   console.log(`CI-PAE backend listening on http://localhost:${port}`)
-  console.log(`局域网访问地址: http://10.47.179.167:${port}`)
+  console.log(`局域网访问地址: http://${serverIP}:${port}`)
 })
